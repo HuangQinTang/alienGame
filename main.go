90ad313f0af0d4c3ebe709fb91e5bc2648eeb87e
HuangQinTang <907236804@qq.com>
2022-12-25 23:33:40 +0800

    闪闪发亮的背景

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
+	"log"
+	"math/rand"
+	"time"
 )
 
-type Game struct{}
+type Game struct {
+	input *Input
+}
+
+func NewGame() *Game {
+	return &Game{
+		input: &Input{msg: "Hello, World!"},
+	}
+}
 
 func (g *Game) Update() error {
+	g.input.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World")
+	rand.Seed(time.Now().UnixNano())
+	screen.Fill(color.RGBA{
+		R: uint8(rand.Intn(254) + 1),
+		G: uint8(rand.Intn(254) + 1),
+		B: uint8(rand.Intn(254) + 1),
+		A: uint8(rand.Intn(254) + 1),
+	})
+	ebitenutil.DebugPrint(screen, g.input.msg)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -24,7 +43,26 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("外星人入侵")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game := NewGame()
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
+	}
+}
+
+type Input struct {
+	msg string
+}
+
+func (i *Input) Update() {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		fmt.Println("←←←←←←←←←←←←←←←←←←←←←←←")
+		i.msg = "left pressed"
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		fmt.Println("→→→→→→→→→→→→→→→→→→→→→→→")
+		i.msg = "right pressed"
+	} else if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		fmt.Println("-----------------------")
+		i.msg = "space pressed"
 	}
 }
